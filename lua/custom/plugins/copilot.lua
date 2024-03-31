aedeb35da420d9985ec5fae599603cc8873f5354
a389750 <oliver.otterlind@volvo.com>
2024-03-31 12:15:33 +0200

    Own config

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,13 @@
+-- Copilot configuration
+--
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    }
+  end,
+}
